541b4dc2b9bd4eb9563538721da1f71b4fe8d9c3
Daniel La <daniel@Daniels-MBP-2.home>
2018-11-06 23:47:09 -0500

    Style and content updates

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,700" rel="stylesheet">
+        <!-- <link rel="apple-touch-icon" href="icon.png"> -->
+        <!-- Place favicon.ico in the root directory -->
+
+        <title>Oo Bar</title>
+        <link rel="stylesheet" href="css/style.min.css">
+    </head>
+    <body> 
+        <main>
+            <header class="header">
+                <div class="header__logo">
+                    <a href="">
+                        <img class="header__logo-image" src="/img/oobar-logo.jpg" alt="Oo bar and lounge logo">
+                    </a>
+                </div>
+
+                <div class="header__nav">
+                    <nav class="navigation">
+                        <ul class="navigation__list">
+                            <li class="navigation__item"><a class="navigation__link" href="">Home</a></li>
+                            <li class="navigation__item"><a class="navigation__link" href="#about-us">About Us</a></li>
+                            <li class="navigation__item"><a class="navigation__link" href="">Upcoming Events</a></li>
+                            <li class="navigation__item"><a class="navigation__link" href="">Event Calendar</a></li>
+                            <li class="navigation__item"><a class="navigation__link" href="#contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <section class="section-banner">
+                <img class="section-banner__img" src="/img/oobar-view.jpg" alt="View from oobars rooftop">
+            </section>
+
+            <section class="about-us flex flex--row flex--wrap">
+                <div class="about-us__desc">
+                    <h2 class="heading-secondary">About Us</h2>
+                    <p>
+                        Located on our 8th and 9th floor, Oo Bar and Lounge provides a trendy and upscale lounge for our guests. 
+                        Here you will find all the luxuries of Manhattan, without the hassle of city transit. 
+                        Enjoy divine appetizers and delicious cocktails in a gorgeous marble stone space, flooded with LED lights. 
+                        No reservations are required for Oo Bar and Lounge. Head upstairs to our rooftop glass house to enjoy sweeping 
+                        views of the Manhattan skyline or to cozy up at one of our handmade fire pits.
+                    </p>
+                    <br />
+                    <p>
+                        For access, please use the designated elevator located on the lobby floor.
+                    </p>
+                </div>
+
+                <div class="upcoming-events">
+                    <h2>Check out our upcoming events</h2>
+                </div>
+            </section>
+            <footer>
+
+            </footer>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        </main>
+    </body>
+</html>
